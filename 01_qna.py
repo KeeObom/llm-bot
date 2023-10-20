8848941631c9ce3dcd772abcc217da31260d6e64
KeeObom <melunatam4@gmail.com>
2023-10-20 12:02:30 +0100

    Upgrade langchain to latest

--- 01_qna.py
+++ 01_qna.py
@@ -1,8 +1,7 @@
 from dotenv import load_dotenv
-
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain import OpenAI
@@ -10,25 +9,26 @@ from langchain import OpenAI
 load_dotenv()
 embeddings = OpenAIEmbeddings()
 
-# loader = TextLoader('news/summary.txt')
-loader = DirectoryLoader('news', glob="**/*.txt")
-
+# Loading texts/documents
+loader = DirectoryLoader('news', glob="**/*.txt") 
 documents = loader.load()
-print(len(documents))
+
+# Splitting Texts like Tokenization
 text_splitter = CharacterTextSplitter(chunk_size=2500, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
-# print(texts)
 
-docsearch = Chroma.from_documents(texts, embeddings)
+# Use the Chroma Vector Store to store embeddings and metadata
+vecstore = Chroma.from_documents(texts, embeddings)
 qa = RetrievalQA.from_chain_type(
-    llm=OpenAI(), 
-    chain_type="stuff", 
-    retriever=docsearch.as_retriever()
+    llm=OpenAI(),
+    chain_type="stuff",
+    retriever=vecstore.as_retriever()
 )
 
 def query(q):
     print("Query: ", q)
     print("Answer: ", qa.run(q))
+
 
 query("What are the effects of legislations surrounding emissions on the Australia coal market?")
 query("What are China's plans with renewable energy?")
